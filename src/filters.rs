78b0001898b9cdd4ed1caa88d425d255ab723aa8
Jade Lovelace <software@lfcode.ca>
2022-07-03 17:39:36 -0700

    less janky re-length

--- src/filters.rs
+++ src/filters.rs
@@ -34,7 +34,7 @@ impl DelayLine {
 
     pub fn set_len(&mut self, new_len: usize) {
         self.samples = vec![0.; new_len];
-        self.pos = new_len.min(self.samples.len()) - 1;
+        self.pos %= new_len.min(self.samples.len());
     }
 }
 
